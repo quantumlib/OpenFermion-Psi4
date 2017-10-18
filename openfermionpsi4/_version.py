2290cbde8836f8f719df6eb8dc83998e355588a5
Jarrod McClean <jmcclean@google.com>
2017-10-18 10:50:05 -0700

    Update version number

--- openfermionpsi4/_version.py
+++ openfermionpsi4/_version.py
@@ -15,4 +15,4 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Define version number here and read it from setup.py automatically"""
-__version__ = "0.1a2"
+__version__ = "0.1a3"
